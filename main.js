14781e717b25563c20c660f8b5b0ac3eb2fff512
Houtinyee <houtinyee@gmail.com>
2026-02-14 02:19:24 +0100

    dont press

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function showMessage() {
-      alert("I'm a chud!");
-    }
+  alert("I'm a chud!");
+}
+function changeImage() {
+  let img = document.getElementById("myImage");
+  img.src = "spookycat.png";
+  if(img == cat.png){
+    img.src = "spookycat.png";
+  }else{
+    img.src = "cat.png";
+  }
+}
